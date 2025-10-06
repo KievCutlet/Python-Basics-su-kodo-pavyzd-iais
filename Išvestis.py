0e97572e98f05c918da4e2dc2aeeb4bb09d0bfbb
KievCutlet <burdenkovassimas@gmail.com>
2025-10-06 22:44:13 +0300

    Create Išvestis.py
    Pirma pamokėle, tikrai nesudetinga, tik vienas "print()" pavizdys, ir jo paaiškinimas :)

---
+++ Išvestis.py
@@ -0,0 +1,4 @@
+#Išvestis, tai lengviausias dalykas Python programavimo kalboje.
+#Štai paprastas išviesties pavizdys:
+print("Labas!")
+#SVARBIAUSIA: visąlaik "print rašosi mažaja, ir aplink tavo norimo teksto turi būti arba "", arba ''
