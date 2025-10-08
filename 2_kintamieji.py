3978e87cd2dc7c8cc7e402a1c272a2b249d58eb6
KievCutlet <burdenkovassimas@gmail.com>
2025-10-08 10:46:56 +0300

    Create 2_kintamieji.py
    Irgi lengvoka pamokele

---
+++ 2_kintamieji.py
@@ -0,0 +1,15 @@
+#kintamieji yra labai svarbus dalykas Python programavimo kalboje
+#Čia yra keli pavyzdžiai:
+a = 1 #Integer tipo kintamasis, sveikasis skaičius
+b = 1.1 #Float tipo kintamasis, gali būti belenkokie skaiciai
+c = "Tekstas" #String tipo kintamasis
+d = False #Boolean, arba "bool" tipo kintamasis. Gali būti tik "False"(Netiesa) ir "True"(Tiesa)
+
+#Su kintamaisiais galima daryti aritmetinius veiksmus
+
+print(a+b) #Yra sudedama a ir b, sudetis
+print(a-b) #Yra atimima b iš a, atimtis
+print(a*b) #Yra dauginama a ir b, daugyba
+print(a/b) #Yra dalinama a iš b, dalyba
+print(a//b) #Dalyba, bet rezultatas sveikas skaičius
+print(a%b) #Dalyba, bet rezultatas yra liekana
